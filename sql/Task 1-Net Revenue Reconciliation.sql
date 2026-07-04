43e1ece5afc92e9aee68ef1479c9fd92a704aeb1
sainikhil <madupusainikhil@gmail.com>
2026-07-04 17:53:45 +0530

    Added SQL Task 1: Net Revenue Reconciliation

--- sql/Task 1-Net Revenue Reconciliation.sql
+++ sql/Task 1-Net Revenue Reconciliation.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE project_sql;
 use project_sql;
+
+
+CREATE TABLE salestransaction (
+    TransactionNo VARCHAR(20),
+    Date VARCHAR(20),
+    ProductNo VARCHAR(20),
+    ProductName VARCHAR(255),
+    Price DECIMAL(10,2),
+    Quantity INT,
+    CustomerNo VARCHAR(20),
+    Country VARCHAR(100)
+);
